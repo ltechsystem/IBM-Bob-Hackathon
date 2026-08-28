c514b25e2cf5d7393ab46c363cc5f709c97ec719
Ltechsystem <joachimlow02@gmail.com>
2026-08-28 20:22:02 +0200

    Description for the agent

---
+++ sentinel/__init__.py
@@ -0,0 +1,9 @@
+"""
+Sentinel - Continuous test maintenance agent for legacy RPG on IBM i.
+
+Package initialisation. Import submodules explicitly; nothing is imported here
+by default to keep startup cost low and avoid requiring IBM i connectivity at
+import time.
+"""
+
+__version__ = "0.1.0"
